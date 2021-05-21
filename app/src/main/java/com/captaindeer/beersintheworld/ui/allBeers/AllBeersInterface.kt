6da2bf4ff494f0c04cb33f732be248894de9f1a3
CaptainDeer <erikhernandez323@gmail.com>
2021-05-21 12:10:59 -0500

    feature/coroutines

--- app/src/main/java/com/captaindeer/beersintheworld/ui/allBeers/AllBeersInterface.kt
+++ app/src/main/java/com/captaindeer/beersintheworld/ui/allBeers/AllBeersInterface.kt
@@ -2,11 +2,12 @@ package com.captaindeer.beersintheworld.ui.allBeers
 
 import com.captaindeer.beersintheworld.data.local.entities.BeerEntity
 import com.captaindeer.beersintheworld.data.remote.responses.BeerResponse
+import com.captaindeer.beersintheworld.ui.bases.BasePresenter
 import com.captaindeer.beersintheworld.ui.bases.BaseView
 
 interface AllBeersInterface {
 
-    interface Presenter {
+    interface Presenter : BasePresenter {
         fun getBeers()
     }
 
--- app/src/main/java/com/captaindeer/beersintheworld/ui/allBeers/AllBeersPresenter.kt
+++ app/src/main/java/com/captaindeer/beersintheworld/ui/allBeers/AllBeersPresenter.kt
@@ -4,60 +4,53 @@ import android.content.Context
 import com.captaindeer.beersintheworld.data.local.LocalDatabase
 import com.captaindeer.beersintheworld.data.local.entities.BeerEntity
 import com.captaindeer.beersintheworld.data.remote.RetrofitBuilder
-import com.captaindeer.beersintheworld.data.remote.responses.BeerResponse
 import com.captaindeer.beersintheworld.utils.OnLine
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class AllBeersPresenter(private val context: Context, private val view: AllBeersInterface.View) :
-    AllBeersInterface.Presenter {
-    private val retrofit = RetrofitBuilder()
+    AllBeersInterface.Presenter, CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext: CoroutineContext = job + Dispatchers.IO
+
+    private var retrofit = RetrofitBuilder()
     private val database = LocalDatabase(context)
 
     override fun getBeers() {
         if (OnLine.isNetworkAvailable(context)) {
-            retrofit.getBeers().enqueue(object : Callback<ArrayList<BeerResponse>> {
-                override fun onResponse(
-                    call: Call<ArrayList<BeerResponse>>,
-                    response: Response<ArrayList<BeerResponse>>
-                ) {
-                    when {
-                        response.code() == 200 -> {
-                            if (response.isSuccessful) {
-                                val beers = response.body()
-                                beers?.forEach { beer ->
-                                    database.productDao().insert(
-                                        BeerEntity(
-                                            beer.id,
-                                            beer.name,
-                                            beer.description,
-                                            beer.image_url,
-                                            beer.brewers_tips,
-                                            beer.contributed_by
-                                        )
-                                    )
-                                }
-                                view.setBeers(database.productDao().updatePosts() as ArrayList)
-                            }
-                        }
-                        response.code() == 404 -> {
-                            view.onError("Something is wrong. Try again please. 404")
-                        }
-                        response.code() == 500 -> {
-                            view.onError("Internal server error. 500")
-                        }
-                    }
-                }
 
-                override fun onFailure(call: Call<ArrayList<BeerResponse>>, t: Throwable) {
-                    view.onError(t.message!!)
+            launch {
+                retrofit = RetrofitBuilder()
+                val response = retrofit.getBeers()
+                if (response.isSuccessful){
+                    response.body()?.forEach { beer ->
+                        database.productDao().insert(
+                            BeerEntity(
+                                beer.id,
+                                beer.name,
+                                beer.description,
+                                beer.image_url,
+                                beer.brewers_tips,
+                                beer.contributed_by
+                            )
+                        )
+                    }
+                    withContext(Dispatchers.Main){
+                        view.setBeers(database.productDao().updatePosts() as ArrayList)
+                    }
+                }else{
+                    view.onError(response.errorBody().toString())
                 }
-            })
+            }
         } else {
             view.onError("No internet detected.")
             view.setBeers(database.productDao().updatePosts() as ArrayList)
         }
+    }
+
+    override fun onCancel() {
+        job.cancel()
     }
 
 }
